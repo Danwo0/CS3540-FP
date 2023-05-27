d72d20c46033f5e817dd5e93172b1f97eee65459
lidmuspaper <90637834+lidmuspaper@users.noreply.github.com>
2023-05-26 20:36:37 -0400

    Recommit

---
+++ Assets/Scripts/PickupBehavior.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupBehavior : MonoBehaviour
+{
+    public float duration = 60f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, duration);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
