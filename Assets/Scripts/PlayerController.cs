0a22619951aba26946a149f286e5eff4eef2b80f
lidmuspaper <90637834+lidmuspaper@users.noreply.github.com>
2023-05-26 21:12:41 -0400

    Simple Inventory
    inventory for powerups, currently only speed boosts

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,59 +4,78 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-        public float moveSpeed = 5f;
-        public float jumpHeight = 2f;
-        public float gravity = 9.81f;
-        public float airControl = 10f;
+    public float moveSpeed = 5f;
+    public float jumpHeight = 2f;
+    public float gravity = 9.81f;
+    public float airControl = 10f;
+    
+    private CharacterController controller;
+    private Vector3 input, moveDirection;
+    private string currentItem = "";
+    private float speedBoost = 1;
+    
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
         
-        private CharacterController controller;
-        private Vector3 input, moveDirection;
-        
-        void Start()
-        {
-            controller = GetComponent<CharacterController>();
-        }
-        
-        void Update()
-        {
-            float moveHorizontal = Input.GetAxis("Horizontal");
-            float moveVertical = Input.GetAxis("Vertical");
-    
-            input = (transform.right * moveHorizontal + transform.forward * moveVertical).normalized;
-    
-            input *= moveSpeed;
-    
-            if (controller.isGrounded)
-            {
-                moveDirection = input;
-                if (Input.GetButton("Jump"))
-                {
-                    moveDirection.y = Mathf.Sqrt(2 * jumpHeight * gravity);
-                }
-                else
-                {
-                    moveDirection.y = 0.0f;
-                }
-            }
-            else
-            {
-                input.y = moveDirection.y;
-                moveDirection = Vector3.Lerp(moveDirection, input, airControl * Time.deltaTime);
-            }
-    
-            moveDirection.y -= gravity * Time.deltaTime;
-            controller.Move(moveDirection * Time.deltaTime);
+    void Update()
+    {
+        PowerUp();
 
-            // crouching
-            if (Input.GetKey(KeyCode.LeftShift))
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+    
+        input = (transform.right * moveHorizontal + transform.forward * moveVertical).normalized;
+        
+        input *= moveSpeed * speedBoost;
+    
+        if (controller.isGrounded)
+        {
+            moveDirection = input;
+            if (Input.GetButton("Jump"))
             {
-                transform.localScale = 
-                    Vector3.Lerp(transform.localScale, new Vector3(1, 0.5f, 1), Time.deltaTime * 10);
+                moveDirection.y = Mathf.Sqrt(2 * jumpHeight * gravity);
             }
             else
             {
-                transform.localScale = 
-                    Vector3.Lerp(transform.localScale, new Vector3(1, 1, 1), Time.deltaTime * 10);
+                moveDirection.y = 0.0f;
             }
         }
+        else
+        {
+            input.y = moveDirection.y;
+            moveDirection = Vector3.Lerp(moveDirection, input, airControl * Time.deltaTime);
+        }
+    
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
+
+        // crouching
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            transform.localScale = 
+                Vector3.Lerp(transform.localScale, new Vector3(1, 0.5f, 1), Time.deltaTime * 10);
+        }
+        else
+        {
+            transform.localScale = 
+                Vector3.Lerp(transform.localScale, new Vector3(1, 1, 1), Time.deltaTime * 10);
+        }
+    }
+
+    public void setCurrentItem(string item)
+    {
+        currentItem = item;
+    }
+    void PowerUp()
+    {
+        if(currentItem == "speed")
+        {
+            speedBoost = 3;
+        } else
+        {
+            speedBoost = 1;
+        }
+    }
 }
