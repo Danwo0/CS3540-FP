b83bd63aef6dea7b269cfa7fb65bbf9bae0cbe02
lidmuspaper <90637834+lidmuspaper@users.noreply.github.com>
2023-06-02 21:25:10 -0400

    Weapon swapping
    weapon swapping

--- Assets/Scripts/ShootProjectile.cs
+++ Assets/Scripts/ShootProjectile.cs
@@ -5,28 +5,62 @@ using UnityEngine.UI;
 public class ShootProjectile : MonoBehaviour
 {
     public GameObject projectilePrefab;
-    public float projectileSpeed = 300f;
-    // public AudioClip spellSFX;
+    public GameObject meleePrefab;
+
+    public float projectileSpeed = 100f;
+    public float meleeSpeed = 0f;
+
+    public AudioClip projectileSFX;
+    public AudioClip meleeSFX;
+
+    int type;
 
     // Start is called before the first frame update
     void Start()
     {
+        type = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetAxis("Mouse ScrollWheel") != 0)
+        {
+            type = (type + 1) % 2;
+        }
+
         if (Input.GetButtonDown("Fire1"))
         {
-            GameObject projectile = Instantiate(projectilePrefab, transform.position + transform.forward, transform.rotation) as GameObject;
-
-            Rigidbody rb = projectile.GetComponent<Rigidbody>();
-
-            rb.AddForce(transform.forward * projectileSpeed, ForceMode.VelocityChange);
-
-            projectile.transform.SetParent(GameObject.FindGameObjectWithTag("ProjectileParent").transform);
-
-            // AudioSource.PlayClipAtPoint(spellSFX, transform.position);
+            if (type == 0)
+            {
+                fireProjectile();
+            } else if (type == 1)
+            {
+                fireMelee();
+            }
         }
+    }
+
+    void fireProjectile()
+    {
+        GameObject projectile = Instantiate(projectilePrefab, transform.position + transform.forward, transform.rotation) as GameObject;
+
+        Rigidbody rb = projectile.GetComponent<Rigidbody>();
+
+        rb.AddForce(transform.forward * projectileSpeed, ForceMode.VelocityChange);
+
+        projectile.transform.SetParent(GameObject.FindGameObjectWithTag("ProjectileParent").transform);
+
+        AudioSource.PlayClipAtPoint(projectileSFX, transform.position);
+    }
+    void fireMelee()
+    {
+        GameObject projectile = Instantiate(meleePrefab, transform.position + transform.forward, transform.rotation) as GameObject;
+
+        Rigidbody rb = projectile.GetComponent<Rigidbody>();
+
+        projectile.transform.SetParent(GameObject.FindGameObjectWithTag("ProjectileParent").transform);
+
+        AudioSource.PlayClipAtPoint(meleeSFX, transform.position);
     }
 }
