f57d753b291c94c7927a8fc3be2f8e9013739a70
Jun Lee <dan.junwoo.lee@gmail.com>
2023-06-17 04:16:04 -0400

    Improved projectile system (#34)
    * Refactored and revamped attack system, new projectile
    
    * removed unnecessary code, level 1 and 2 updated
    
    * remove merge conflicts
    
    * remove unused script

--- Assets/Scripts/Player/ThirdPersonShooterController.cs
+++ Assets/Scripts/Player/ThirdPersonShooterController.cs
@@ -9,34 +9,20 @@ public class ThirdPersonShooterController : MonoBehaviour
     public CinemachineVirtualCamera aimVirtualCamera;
     public LayerMask aimColliderMask = new LayerMask();
 
-    public Transform projectileOrigin;
-    public GameObject projectilePrefab;
-    public GameObject meleePrefab;
-    
-    public AudioClip projectileSFX;
-    public AudioClip meleeSFX;
-    public AudioClip jumpSFX;
-    public AudioClip landSFX;
-
-    public Image selectionImage;
-    public Sprite gunSprite;
-    public Sprite meleeSprite;
-
     public bool isDebugging = false;
     public Transform debug;
 
     public float normalSensitivity = 1f;
     public float aimSensitivity = 0.6f;
-    public float projectileSpeed = 100f;
-    public float shootingAlertRadius = 20f;
 
     private Animator anim;
     
     private CameraController mouseLook;
     private ThirdPersonMovementController controller;
-    private Vector3 mouseWorldPosition;
     
-    private bool isAiming;
+    public Vector3 mouseWorldPosition;
+    public bool isAiming;
+    
     private int weaponType;
 
     void Start()
@@ -46,12 +32,10 @@ public class ThirdPersonShooterController : MonoBehaviour
         anim = GetComponent<Animator>();
         
         isAiming = false;
-        weaponType = 0;
-        selectionImage.sprite = gunSprite;
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
         if (LevelManager.isGameOver) return;
         
@@ -61,8 +45,6 @@ public class ThirdPersonShooterController : MonoBehaviour
 
         UpdatePointer();
         Aiming();
-        SwitchWeapon();
-        Attack();
     }
 
     void UpdatePointer()
@@ -102,105 +84,5 @@ public class ThirdPersonShooterController : MonoBehaviour
             controller.SetRotateOnMove(true);
             isAiming = false;
         }
-    }
-
-    void SwitchWeapon()
-    {
-        if (Input.GetAxis("Mouse ScrollWheel") != 0)
-        {
-            weaponType = (weaponType + 1) % 2;
-            if (weaponType == 0)
-            {
-                selectionImage.sprite = gunSprite;
-            }
-            else if (weaponType == 1)
-            {
-                selectionImage.sprite = meleeSprite;
-            }
-        }
-    }
-
-    void Attack()
-    {
-        if (anim.GetCurrentAnimatorStateInfo(0).IsName("Idle_Walk_Run") && Input.GetButtonDown("Fire1"))
-        {
-            if (weaponType == 0)
-            {
-                anim.SetBool("attackRanged", true);
-                Invoke("FireProjectile", 0.2f);
-                AlertNearby();
-            }
-            else if (weaponType == 1)
-            {
-                anim.SetBool("attackMelee", true);
-                Invoke("FireMelee", 0.2f);
-            }
-        }
-    }
-    
-    void FireProjectile()
-    {
-        Quaternion bulletDirection = Quaternion.identity;
-        if (isAiming)
-        {
-            Vector3 aimDirection = (mouseWorldPosition - projectileOrigin.position).normalized;
-            bulletDirection = Quaternion.LookRotation(aimDirection, Vector3.up);
-        }
-        else
-        {
-            bulletDirection = transform.rotation;
-        }
-        
-        GameObject projectile = Instantiate
-            (projectilePrefab, projectileOrigin.position, bulletDirection) as GameObject;
-            
-        Rigidbody rb = projectile.GetComponent<Rigidbody>();
-        rb.AddForce(projectile.transform.forward * projectileSpeed, ForceMode.VelocityChange);
-
-        projectile.transform.SetParent(GameObject.FindGameObjectWithTag("ProjectileParent").transform);
-
-        AudioSource.PlayClipAtPoint(projectileSFX, transform.position);
-    }
-
-    void FireMelee()
-    {
-        GameObject projectile = Instantiate
-            (meleePrefab, projectileOrigin.position + transform.forward * 2, transform.rotation) as GameObject;
-
-        Rigidbody rb = projectile.GetComponent<Rigidbody>();
-        rb.AddForce(transform.forward, ForceMode.VelocityChange);
-
-        projectile.transform.SetParent(GameObject.FindGameObjectWithTag("ProjectileParent").transform);
-
-        AudioSource.PlayClipAtPoint(meleeSFX, transform.position);
-    }
-    
-    void AlertNearby()
-    {
-        Collider[] others =  Physics.OverlapSphere(transform.position, shootingAlertRadius);
-
-        foreach(Collider other in others)
-        {
-            if (other.gameObject.CompareTag("Astronaut"))
-            {
-                AstronautAI astronaut = other.gameObject.GetComponent<AstronautAI>();
-                if (astronaut.currentState == AstronautAI.FSMStates.Idle ||
-                    astronaut.currentState == AstronautAI.FSMStates.Patrol)
-                    astronaut.GunshotAlert();
-            }
-            if (other.gameObject.CompareTag("Robot"))
-            {
-                RobotAI robot = other.gameObject.GetComponent<RobotAI>();
-                if (robot.currentState == RobotAI.FSMStates.Idle || 
-                    robot.currentState == RobotAI.FSMStates.Patrol) 
-                    robot.GunshotAlert();
-            }
-        }
-    }
-
-    void OnDrawGizmos()
-    {
-        Gizmos.color = Color.magenta;
-        Gizmos.DrawWireSphere(transform.position, shootingAlertRadius);
     }
 }
