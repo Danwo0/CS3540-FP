892f6f0bb2b1a8b138d55c4d9ab6d27fa39367c7
lidmuspaper <90637834+lidmuspaper@users.noreply.github.com>
2023-06-22 00:12:51 -0400

    fv

--- Assets/LoadFirstLevel.cs
+++ Assets/LoadFirstLevel.cs
@@ -5,8 +5,15 @@ using UnityEngine.SceneManagement;
 
 public class LoadFirstLevel : MonoBehaviour
 {
+    private void Start()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
     public void LoadFirst()
     {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
         SceneManager.LoadScene(1);
     }
 }
